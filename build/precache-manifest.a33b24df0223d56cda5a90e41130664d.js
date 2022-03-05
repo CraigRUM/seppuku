fe7f6522c20751f5b072e225ce17aecf6f5262cd
craig <craigrum@gmail.com>
2022-03-05 20:08:08 +0000

    Deploy React app to GitHub Pages

---
+++ build/precache-manifest.a33b24df0223d56cda5a90e41130664d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5eb586d88c726294e86c9ceaaaccde",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2579ed0348617d3118fc",
+    "url": "/react-gh-pages/static/css/main.12f5ba98.chunk.css"
+  },
+  {
+    "revision": "160dd4514e7c21da62b0",
+    "url": "/react-gh-pages/static/js/2.138ad11c.chunk.js"
+  },
+  {
+    "revision": "b65901605a14dc72bc18",
+    "url": "/react-gh-pages/static/js/3.1ef263b0.chunk.js"
+  },
+  {
+    "revision": "2579ed0348617d3118fc",
+    "url": "/react-gh-pages/static/js/main.a54e6751.chunk.js"
+  },
+  {
+    "revision": "e8c855d5d7891b0c5963",
+    "url": "/react-gh-pages/static/js/runtime~main.b06caae1.js"
+  },
+  {
+    "revision": "3fd11e651bfa2b27e693e29473b47cf8",
+    "url": "/react-gh-pages/static/media/bin.3fd11e65.svg"
+  },
+  {
+    "revision": "c4363820f5b362bafaf078b5a9d46906",
+    "url": "/react-gh-pages/static/media/camera.c4363820.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  }
+]);
