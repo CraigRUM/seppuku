4e473789dd1347eafe1ebfc6b5f058466d1cfc98
craig <craigrum@gmail.com>
2022-03-08 17:25:01 +0000

    updated build with screen snap

---
+++ build/precache-manifest.f65f25e595231c7d75ce4a167991b17a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc73b956c19ae9ed50821f90e7c7cad",
+    "url": "/seppuku/index.html"
+  },
+  {
+    "revision": "131d995ec960211f7d7d",
+    "url": "/seppuku/static/css/main.958d00f5.chunk.css"
+  },
+  {
+    "revision": "4538379de87fee090cf8",
+    "url": "/seppuku/static/js/2.98f04a14.chunk.js"
+  },
+  {
+    "revision": "b65901605a14dc72bc18",
+    "url": "/seppuku/static/js/3.1ef263b0.chunk.js"
+  },
+  {
+    "revision": "131d995ec960211f7d7d",
+    "url": "/seppuku/static/js/main.7670e3b9.chunk.js"
+  },
+  {
+    "revision": "3de0cc8a573ddc6b0083",
+    "url": "/seppuku/static/js/runtime~main.bafe2ee9.js"
+  },
+  {
+    "revision": "3fd11e651bfa2b27e693e29473b47cf8",
+    "url": "/seppuku/static/media/bin.3fd11e65.svg"
+  },
+  {
+    "revision": "c4363820f5b362bafaf078b5a9d46906",
+    "url": "/seppuku/static/media/camera.c4363820.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/seppuku/static/media/logo.06e73328.svg"
+  }
+]);
